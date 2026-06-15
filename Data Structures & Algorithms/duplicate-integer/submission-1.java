d42bda5a77a02de278dd3b1b3770b5006b6d0405
Pradeep Yadav <78315659+prads46@users.noreply.github.com>
2026-06-15 08:36:28 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        return nums.length == set.size() ? false : true;
+    }
+}
