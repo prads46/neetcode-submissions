28afe11461e53e99b7affd10e7fd25d0d23c88cf
Pradeep Yadav <78315659+prads46@users.noreply.github.com>
2026-07-11 22:33:00 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        if(stones.length == 1){
+            return stones[0];
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<stones.length;i++){
+            pq.add(stones[i]);
+        }
+
+        while(pq.size() > 1){
+            int x = pq.poll();
+            int y = pq.poll();
+            int curr = Math.abs(x-y);
+            pq.add(curr);
+        }
+
+        return pq.size() == 0 ? 0 : pq.poll();
+    }
+}
